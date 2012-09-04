61db9103384931c65b784dee23617ca57d874b50
Tim Cooper <coop@latrobest.com>
2012-09-04 11:25:37 +1000

    Add cap tasks that we usually use.

--- lib/super_mega_ultra_capistrano_extensions/recipes/deploy.rb
+++ lib/super_mega_ultra_capistrano_extensions/recipes/deploy.rb
@@ -9,15 +9,76 @@ Capistrano::Configuration.instance(:must_exist).load do
   set :user,                'edhdev'
   set :use_sudo,            false
   set :rake,                "bundle exec rake"
-  set :config_files,        fetch(:config_files) { [] }
-  set :job_runner?,         fetch(:job_runner, false)
-  set :migrate?,            fetch(:run_migrations, false)
+  set :config_files,        fetch(:config_files)   { Array.new }
+  set :job_runner?,         fetch(:job_runner)     { false }
+  set :branch,              fetch(:branch)         { 'master' }
+  set :run_migrations,      fetch(:run_migrations) { false }
   set :group_writable,      false
+  set :server_mappings,     {
+    'development' => fetch(:development_servers),
+    'dev'         => fetch(:development_servers),
+    'd'           => fetch(:development_servers),
+    'staging'     => fetch(:staging_servers),
+    's'           => fetch(:staging_servers),
+    'production'  => fetch(:production_servers),
+    'prod'        => fetch(:production_servers),
+    'p'           => fetch(:production_servers)
+  }
+  set :environment,         fetch(:environment, 'development')
+  set :servers,             Array(server_mappings[environment])
 
-  after "deploy:update_code", "deploy:migrate" if migrate?
   after "deploy:restart", "deploy:cleanup"
 
+  role :web, *servers
+  role :app, *servers
+  role :db,  :primary => true do |role|
+    servers
+  end
+
   namespace :deploy do
+    desc <<-DESC
+      Override the default capistrano setup task to either:
+        * setup the application if no current directory
+        * deploy and run migrations if migrations enabled
+        * deploy without migrations
+    DESC
+    task :default do
+      if latest_release
+        if run_migrations
+          migrations
+        else
+          update
+          restart
+        end
+      else
+        setup
+      end
+    end
+
+    desc <<-DESC
+      Override the default capistrano setup because folder structure \
+      is taken care of by puppet. This task currently:
+        * update code
+        * symlink
+        * bundle install
+        * loads database schema
+        * run any migrations
+    DESC
+    task :setup, :except => {:no_release => true} do
+      set :latest_release, release_path
+
+      transaction do
+        update
+
+        if run_migrations
+          load_schema
+          migrate
+        end
+      end
+
+      restart
+    end
+
     desc <<-DESC
       Start the web application. This task currently:
         * starts app server
@@ -46,6 +107,15 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :restart do
       app_server.restart
       job_runner.restart if job_runner?
+    end
+
+    desc <<-DESC
+      Load the database schema from db/schema.rb for the current \
+      environment. This will give you the lastest schema without \
+      running migrations.
+    DESC
+    task :load_schema, :roles => :db, :only => {:primary => true} do
+      run "cd #{latest_release} && RAILS_ENV=#{rails_env} #{rake} db:schema:load"
     end
   end
 end
